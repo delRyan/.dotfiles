4590af9c1069ef355a2c0c2968838f95cae5aea7
Ryan Delaney <ryan.del17@gmail.com>
2024-08-22 21:32:35 -0700

    Clean up zshrc

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,20 +5,9 @@ alias ....='cd ../../..'
 alias .....='cd ../../../..'
 alias 'bk=cd $OLDPWD'
 
-# remote machines
-alias sshd='ssh ryandel.aka.corp.amazon.com'
-
 # alias to cat this file to display
 alias acat='< ~/.zsh/aliases.zsh'
 alias fcat='< ~/.zsh/functions.zsh'
 alias sz='source ~/.zshrc'
 
-# Brazil
-alias bb=brazil-build
-alias bbc="brazil-build clean"
-alias bbt="brazil-build test"
-alias bbr="brazil-build release"
-alias bbs="brazil-build server"
-
-# Other
-alias ninja=ninja-dev-sync
+alias k='kubectl'
