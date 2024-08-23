3f2ff669937bc249fb3a4960fc2175f6672f4b8d
delRyan <ryan.del17@gmail.com>
2024-08-22 21:32:35 -0700

    Clean up zshrc

--- zsh/amazon.zsh
+++ zsh/amazon.zsh
@@ -1,14 +1,21 @@
-# --Exports--
 export PATH="/apollo/env/SDETools/bin:$PATH"
 
-# --Functions--
- # Runs a single unit test class.
- bbst() {
+alias sshd='ssh ryandel.aka.corp.amazon.com'
+
+alias bb=brazil-build
+alias bbc="brazil-build clean"
+alias bbt="brazil-build test"
+alias bbr="brazil-build release"
+alias bbs="brazil-build server"
+
+alias ninja=ninja-dev-sync
+
+# Runs a single unit test class.
+bbst() {
          CLASS=`echo $1 | perl -pe 's/\//./gi; s/\.java$//; s/^.*\.(?=com\.)//'`
           echo "\n> Running single test for $CLASS\n"
           brazil-build single-test -DtestClass $CLASS
  }
 
-# --Other--
 # Increase maximum number of open files per shell.
 ulimit -S -n 8192
