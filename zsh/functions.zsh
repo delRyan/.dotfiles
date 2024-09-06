830b53fe65838b55133afd91f797e2c47f861d22
Ryan Delaney <ryan.del17@gmail.com>
2024-09-05 20:34:13 -0700

    Clean up zsh functions

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,34 +1,10 @@
-# -------------------------------------------------------------------
-# Count code lines in some directory.
-# $ loc py js css
-# # => Lines of code for .py: 3781
-# # => Lines of code for .js: 3354
-# # => Lines of code for .css: 2970
-# # => Total lines of code: 10105
-# from https://github.com/paulmillr/dotfiles
-# -------------------------------------------------------------------
-function countcode() {
-  local total
-  local firstletter
-  local ext
-  local lines
-  total=0
-  for ext in $@; do
-    firstletter=$(echo $ext | cut -c1-1)
-    if [[ firstletter != "." ]]; then
-      ext=".$ext"
-    fi
-    lines=`find-exec "*$ext" cat | wc -l`
-    lines=${lines// /}
-    total=$(($total + $lines))
-    echo "Lines of code for ${fg[blue]}$ext${reset_color}: ${fg[green]}$lines${reset_color}"
-  done
-  echo "${fg[blue]}Total${reset_color} lines of code: ${fg[green]}$total${reset_color}"
+# Visualize ansi color escape codes for terminal setup
+function colormap() {
+  for i in {0..255}; do print -Pn "%K{$i}  %k%F{$i}${(l:3::0:)i}%f " ${${(M)$((i%6)):#3}:+$'\n'}; done
 }
-# -------------------------------------------------------------------
-# display a neatly formatted path
-# -------------------------------------------------------------------
-path() {
+
+# Display a neatly formatted $PATH
+function path() {
   echo $PATH | tr ":" "\n" | \
     awk "{ sub(\"/usr\",   \"$fg_no_bold[green]/usr$reset_color\"); \
            sub(\"/bin\",   \"$fg_no_bold[blue]/bin$reset_color\"); \
