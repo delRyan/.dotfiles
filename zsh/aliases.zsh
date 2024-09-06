bea54e2c9797128d9f0ab3b98daa427e43f064e2
Ryan Delaney <ryan.del17@gmail.com>
2024-09-05 20:59:36 -0700

    Add eza

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,9 +5,14 @@ alias ....='cd ../../..'
 alias .....='cd ../../../..'
 alias 'bk=cd $OLDPWD'
 
+alias ls="eza --icons --group-directories-first"
+alias ll="eza --icons --group-directories-first -l"
+
 # alias to cat this file to display
 alias acat='< ~/.zsh/aliases.zsh'
 alias fcat='< ~/.zsh/functions.zsh'
 alias sz='source ~/.zshrc'
 
 alias k='kubectl'
+
+alias code="open -a 'Visual Studio Code'"
