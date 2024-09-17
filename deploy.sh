6dc5e8cf5163d94b608fea50c6ae5929c99e3cf7
Ryan Delaney <ryan.del17@gmail.com>
2024-09-16 19:04:32 -0700

    Remove Warp

--- deploy.sh
+++ deploy.sh
@@ -13,9 +13,6 @@ ln -sf $PWD/vim/vimrc ~/.vimrc
 
 ln -sf $PWD/tmux/.tmux.conf ~/.tmux.conf
 
-rm -rf ~/.warp
-ln -s $PWD/warp ~/.warp
-
 # Don't disturb other apps using ~/.config
 mkdir -p ~/.config
 ln -sf $PWD/config/* ~/.config/
